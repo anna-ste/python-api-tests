5490dac5f059d287786c51fd4488e99c33f68cf9
anna-ste <anna.ste0502@gmail.com>
2022-06-06 23:17:31 +0300

    Add retrieve trading pair steps

--- features/steps/server_time.py
+++ features/steps/server_time.py
@@ -3,12 +3,13 @@ from datetime import datetime
 import requests
 from behave import given, when, then
 
-from src.utils import response_handler
+from src.utils import response_handler, schema_loader
 from src.utils.settings_parser import settings
 
 base_url = settings.base_url
 api_key = settings.api_key
 api_secret = settings.api_secret
+trading_pairs = settings.trading_pairs
 
 
 @when(u'I retrieve server time')
@@ -34,3 +35,26 @@ def step_impl(context):
 def assert_status_code(context, code):
     actual_code = response_handler.status_code(context.response)
     assert actual_code == code, 'Invalid status code, expected %s, actual code is %s' % (code, actual_code)
+
+
+@when(u'I request public info about trading pair {pair}')
+def step_impl(context, pair):
+    pair_parameter = trading_pairs[pair]['request_parameter']
+    context.response = requests.get(base_url + f'/0/public/AssetPairs?pair={pair_parameter}')
+
+
+@then(u'Response data should be about {pair} trading pair')
+def step_impl(context, pair):
+    expected_altname = trading_pairs[pair]['altname']
+    actual_altname = context.response.json()['result'][trading_pairs[pair]['request_parameter']]['altname']
+    assert expected_altname == actual_altname, 'Unexpected trading pair in response, expected %s, actual pair %s' % (
+    expected_altname, actual_altname)
+
+
+@then("{name} json schema should be valid")
+def step_impl(context, name):
+
+    schema = schema_loader.get_json_schema(schema=name.lower())
+
+    # check response corresponds to schema
+    assert response_handler.validate_json(context.resp, schema), 'Category list schema is not valid'
