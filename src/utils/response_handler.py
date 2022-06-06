a364e8a03aa3a066d47f60c5e355ffadbac9f3e8
anna-ste <anna.ste0502@gmail.com>
2022-06-06 23:15:48 +0300

    Add validate schema in response handler

--- src/utils/response_handler.py
+++ src/utils/response_handler.py
@@ -4,3 +4,7 @@ import jsonschema
 def status_code(response):
     return str(response.status_code)
 
+
+def validate_json(response, schema):
+    jsonschema.validate(instance=response.json(), schema=schema)
+    return True
