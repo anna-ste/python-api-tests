397b435dcc658c7bdb148ad04de34f01a94d406f
anna-ste <anna.ste0502@gmail.com>
2022-06-07 00:23:34 +0300

    Update server time feature

--- features/steps/server_time.py
+++ features/steps/server_time.py
@@ -1,8 +1,10 @@
+import time
 from datetime import datetime
 
 import requests
 from behave import given, when, then
 
+from src.utils import request_helper
 from src.utils import response_handler, schema_loader
 from src.utils.settings_parser import settings
 
@@ -10,6 +12,7 @@ base_url = settings.base_url
 api_key = settings.api_key
 api_secret = settings.api_secret
 trading_pairs = settings.trading_pairs
+otp = settings.otp
 
 
 @when(u'I retrieve server time')
@@ -27,7 +30,6 @@ def assert_time_format(context, time_format):
 
 @given(u'I am a user')
 def step_impl(context):
-    # raise NotImplementedError(u'STEP: Given I am a user')
     pass
 
 
@@ -48,13 +50,33 @@ def step_impl(context, pair):
     expected_altname = trading_pairs[pair]['altname']
     actual_altname = context.response.json()['result'][trading_pairs[pair]['request_parameter']]['altname']
     assert expected_altname == actual_altname, 'Unexpected trading pair in response, expected %s, actual pair %s' % (
-    expected_altname, actual_altname)
+        expected_altname, actual_altname)
 
 
 @then("{name} json schema should be valid")
 def step_impl(context, name):
-
     schema = schema_loader.get_json_schema(schema=name.lower())
 
     # check response corresponds to schema
-    assert response_handler.validate_json(context.resp, schema), 'Category list schema is not valid'
+    assert response_handler.validate_json(context.response, schema), 'Category list schema is not valid'
+
+
+@given(u'I am an authenticated user')
+def step_impl(context):
+    pass
+
+
+@when(u'I retrieve open orders')
+def step_impl(context):
+    # Construct the request and print the result
+    context.response = request_helper.kraken_post_request(base_url, '/0/private/OpenOrders', {
+        "nonce": str(int(1000 * time.time())),
+        "otp": otp,
+        "trades": True
+    }, api_key, api_secret)
+
+
+@then(u'I should get empty list of opened orders')
+def step_impl(context):
+    expected_list = {}
+    assert context.response.json()['result']['open'] == expected_list, 'List of orders is not empty'
