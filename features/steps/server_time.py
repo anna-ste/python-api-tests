6a9d8aa43008e98d61ee7475ddd329c38074edc6
anna-ste <anna.ste0502@gmail.com>
2022-06-06 23:07:55 +0300

    Refactor server time steps names

--- features/steps/server_time.py
+++ features/steps/server_time.py
@@ -12,12 +12,12 @@ api_secret = settings.api_secret
 
 
 @when(u'I retrieve server time')
-def step_impl(context):
+def retrieve_server_time(context):
     context.response = requests.get(base_url + '/0/public/Time')
 
 
 @then(u'Server time should have {time_format} time format')
-def step_impl(context, time_format):
+def assert_time_format(context, time_format):
     try:
         datetime.strptime(context.response.json()['result']['rfc1123'], time_format)
     except ValueError:
@@ -31,6 +31,6 @@ def step_impl(context):
 
 
 @then(u'I should get {code} status code')
-def step_impl(context, code):
+def assert_status_code(context, code):
     actual_code = response_handler.status_code(context.response)
     assert actual_code == code, 'Invalid status code, expected %s, actual code is %s' % (code, actual_code)
